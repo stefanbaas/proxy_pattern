bb57ca0af41699ae79978a6d6da1959535eeb7c8
Stefan Blaas <stefanbaasweb@gmail.com>
2018-07-12 10:46:54 +0200

    succesful test - testInstantiation - ImageTest

---
+++ Tests/Unit/Tests/ImageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use App\Classes\Image;
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
+{
+    private $sut;
+
+    protected function setUp() /* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+        $this->sut = new Image();
+    }
+
+    public function testInstantiation () : void
+    {
+        $this->assertNotNull($this->sut);
+    }
+}
